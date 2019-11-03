115dc88dcf6515de1856564116fac667152a8195
Vasiliy Vasilyuk <xorcare@gmail.com>
2019-11-03 11:19:28 +0300

    feat: create functions to get a pointer from a base type
    Package pointer contains helper routines for simplifying
    the creation of optional fields of basic type.

--- pointer.go
+++ pointer.go
@@ -3,3 +3,89 @@
 // license that can be found in the LICENSE file.
 
 package pointer
+
+import "reflect"
+
+// Any is a helper routine that allocates a new interface value
+// to store v and returns a pointer to it.
+func Any(v interface{}) interface{} {
+	r := reflect.New(reflect.TypeOf(v))
+	reflect.ValueOf(r.Interface()).Elem().Set(reflect.ValueOf(v))
+	return r.Interface()
+}
+
+// Bool is a helper routine that allocates a new bool value
+// to store v and returns a pointer to it.
+func Bool(v bool) *bool { return &v }
+
+// Byte is a helper routine that allocates a new byte value
+// to store v and returns a pointer to it.
+func Byte(v byte) *byte { return &v }
+
+// Complex64 is a helper routine that allocates a new complex64 value
+// to store v and returns a pointer to it.
+func Complex64(v complex64) *complex64 { return &v }
+
+// Complex128 is a helper routine that allocates a new complex128 value
+// to store v and returns a pointer to it.
+func Complex128(v complex128) *complex128 { return &v }
+
+// Float32 is a helper routine that allocates a new float32 value
+// to store v and returns a pointer to it.
+func Float32(v float32) *float32 { return &v }
+
+// Float64 is a helper routine that allocates a new float64 value
+// to store v and returns a pointer to it.
+func Float64(v float64) *float64 { return &v }
+
+// Int is a helper routine that allocates a new int value
+// to store v and returns a pointer to it.
+func Int(v int) *int { return &v }
+
+// Int8 is a helper routine that allocates a new int8 value
+// to store v and returns a pointer to it.
+func Int8(v int8) *int8 { return &v }
+
+// Int16 is a helper routine that allocates a new int16 value
+// to store v and returns a pointer to it.
+func Int16(v int16) *int16 { return &v }
+
+// Int32 is a helper routine that allocates a new int32 value
+// to store v and returns a pointer to it.
+func Int32(v int32) *int32 { return &v }
+
+// Int64 is a helper routine that allocates a new int64 value
+// to store v and returns a pointer to it.
+func Int64(v int64) *int64 { return &v }
+
+// Rune is a helper routine that allocates a new rune value
+// to store v and returns a pointer to it.
+func Rune(v rune) *rune { return &v }
+
+// String is a helper routine that allocates a new string value
+// to store v and returns a pointer to it.
+func String(v string) *string { return &v }
+
+// Uint is a helper routine that allocates a new uint value
+// to store v and returns a pointer to it.
+func Uint(v uint) *uint { return &v }
+
+// Uint8 is a helper routine that allocates a new uint8 value
+// to store v and returns a pointer to it.
+func Uint8(v uint8) *uint8 { return &v }
+
+// Uint16 is a helper routine that allocates a new uint16 value
+// to store v and returns a pointer to it.
+func Uint16(v uint16) *uint16 { return &v }
+
+// Uint32 is a helper routine that allocates a new uint32 value
+// to store v and returns a pointer to it.
+func Uint32(v uint32) *uint32 { return &v }
+
+// Uint64 is a helper routine that allocates a new uint64 value
+// to store v and returns a pointer to it.
+func Uint64(v uint64) *uint64 { return &v }
+
+// Uintptr is a helper routine that allocates a new uintptr value
+// to store v and returns a pointer to it.
+func Uintptr(v uintptr) *uintptr { return &v }
---
+++ pointer_test.go
@@ -0,0 +1,161 @@
+// Copyright © 2019, Vasiliy Vasilyuk. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pointer
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func equal(t testing.TB, expected, actual interface{}) {
+	if h, ok := t.(interface{ Helper() }); ok {
+		h.Helper()
+	}
+
+	t.Logf("expected: %q, actual: %q", fmt.Sprint(expected), fmt.Sprint(actual))
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("values obtained are not equal, expected: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestAny(t *testing.T) {
+	var _ *bool = Any(true).(*bool)
+	var _ *byte = Any(byte(1)).(*byte)
+	var _ *complex64 = Any(complex64(1.1)).(*complex64)
+	var _ *complex128 = Any(complex128(1.1)).(*complex128)
+	var _ *float32 = Any(float32(1.1)).(*float32)
+	var _ *float64 = Any(float64(1.1)).(*float64)
+	var _ *int = Any(int(1)).(*int)
+	var _ *int8 = Any(int8(8)).(*int8)
+	var _ *int16 = Any(int16(16)).(*int16)
+	var _ *int32 = Any(int32(32)).(*int32)
+	var _ *int64 = Any(int64(64)).(*int64)
+	var _ *rune = Any(rune(1)).(*rune)
+	var _ *string = Any("ptr").(*string)
+	var _ *uint = Any(uint(1)).(*uint)
+	var _ *uint8 = Any(uint8(8)).(*uint8)
+	var _ *uint16 = Any(uint16(16)).(*uint16)
+	var _ *uint32 = Any(uint32(32)).(*uint32)
+	var _ *uint64 = Any(uint64(64)).(*uint64)
+	var _ *uintptr = Any(uintptr(64)).(*uintptr)
+
+	equal(t, true, *Any(true).(*bool))
+	equal(t, false, *Any(false).(*bool))
+	equal(t, true, *(*Any(Bool(true)).(**bool)))
+	equal(t, int64(1), *Any(int64(1)).(*int64))
+	equal(t, uint64(1), *Any(uint64(1)).(*uint64))
+	equal(t, struct{}{}, *Any(struct{}{}).(*struct{}))
+	equal(t, string("pointer"), *Any(string("pointer")).(*string))
+	equal(t, reflect.Method{}, *Any(reflect.Method{}).(*reflect.Method))
+	equal(t, reflect.Method{Name: "name"}, *Any(reflect.Method{Name: "name"}).(*reflect.Method))
+}
+
+func TestBool(t *testing.T) {
+	equal(t, true, *Bool(true))
+	equal(t, false, *Bool(false))
+}
+
+func TestByte(t *testing.T) {
+	equal(t, byte(0), *Byte(byte(0)))
+	equal(t, byte(1), *Byte(byte(1)))
+}
+
+func TestComplex64(t *testing.T) {
+	equal(t, complex64(0), *Complex64(complex64(0)))
+	equal(t, complex64(1), *Complex64(complex64(1)))
+}
+
+func TestComplex128(t *testing.T) {
+	equal(t, complex128(0), *Complex128(complex128(0)))
+	equal(t, complex128(1), *Complex128(complex128(1)))
+}
+
+func TestFloat32(t *testing.T) {
+	equal(t, float32(-1.1), *Float32(float32(-1.1)))
+	equal(t, float32(-1), *Float32(float32(-1)))
+	equal(t, float32(0.0), *Float32(float32(0.0)))
+	equal(t, float32(1), *Float32(float32(1)))
+	equal(t, float32(1.1), *Float32(float32(1.1)))
+}
+
+func TestFloat64(t *testing.T) {
+	equal(t, float64(-1.1), *Float64(float64(-1.1)))
+	equal(t, float64(-1), *Float64(float64(-1)))
+	equal(t, float64(0.0), *Float64(float64(0.0)))
+	equal(t, float64(1), *Float64(float64(1)))
+	equal(t, float64(1.1), *Float64(float64(1.1)))
+}
+
+func TestInt(t *testing.T) {
+	equal(t, int(-1), *Int(int(-1)))
+	equal(t, int(0), *Int(int(0)))
+	equal(t, int(1), *Int(int(1)))
+}
+
+func TestInt8(t *testing.T) {
+	equal(t, int8(-1), *Int8(int8(-1)))
+	equal(t, int8(0), *Int8(int8(0)))
+	equal(t, int8(1), *Int8(int8(1)))
+}
+
+func TestInt16(t *testing.T) {
+	equal(t, int16(-1), *Int16(int16(-1)))
+	equal(t, int16(0), *Int16(int16(0)))
+	equal(t, int16(1), *Int16(int16(1)))
+}
+
+func TestInt32(t *testing.T) {
+	equal(t, int32(-1), *Int32(int32(-1)))
+	equal(t, int32(0), *Int32(int32(0)))
+	equal(t, int32(1), *Int32(int32(1)))
+}
+
+func TestInt64(t *testing.T) {
+	equal(t, int64(-1), *Int64(int64(-1)))
+	equal(t, int64(0), *Int64(int64(0)))
+	equal(t, int64(1), *Int64(int64(1)))
+}
+
+func TestRune(t *testing.T) {
+	equal(t, rune(0), *Rune(rune(0)))
+	equal(t, rune(1), *Rune(rune(1)))
+}
+
+func TestString(t *testing.T) {
+	equal(t, string(""), *String(string("")))
+	equal(t, string("pointer"), *String(string("pointer")))
+}
+
+func TestUint(t *testing.T) {
+	equal(t, uint(0), *Uint(uint(0)))
+	equal(t, uint(1), *Uint(uint(1)))
+}
+
+func TestUint8(t *testing.T) {
+	equal(t, uint8(0), *Uint8(uint8(0)))
+	equal(t, uint8(1), *Uint8(uint8(1)))
+}
+
+func TestUint16(t *testing.T) {
+	equal(t, uint16(0), *Uint16(uint16(0)))
+	equal(t, uint16(1), *Uint16(uint16(1)))
+}
+
+func TestUint32(t *testing.T) {
+	equal(t, uint32(0), *Uint32(uint32(0)))
+	equal(t, uint32(1), *Uint32(uint32(1)))
+}
+
+func TestUint64(t *testing.T) {
+	equal(t, uint64(0), *Uint64(uint64(0)))
+	equal(t, uint64(1), *Uint64(uint64(1)))
+}
+
+func TestUintptr(t *testing.T) {
+	equal(t, uintptr(0), *Uintptr(uintptr(0)))
+	equal(t, uintptr(1), *Uintptr(uintptr(1)))
+}
