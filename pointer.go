918e964260d7157d7edb66cb7acc6c0cc4792a9b
Vasiliy Vasilyuk <xorcare@gmail.com>
2019-11-03 11:19:28 +0300

    Inception | Your mind is the scene of the crime

---
+++ pointer.go
@@ -0,0 +1,5 @@
+// Copyright © 2019, Vasiliy Vasilyuk. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pointer
